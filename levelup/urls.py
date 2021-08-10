03d7e9f12dfd4bcafb945b167f9de39ba03163ca
hann <hmweeks93@gmail.com>
2021-08-10 15:44:47 -0500

    500 error for getting games; still not working

--- levelup/urls.py
+++ levelup/urls.py
@@ -13,32 +13,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include
-from django.urls import path
+
+from levelupapi.models.game import Game
+from levelupapi.models import game_type
+from django.contrib import admin
+from django.urls import include, path
 from levelupapi.views import register_user, login_user
 from rest_framework import routers
-from levelupapi.views import GameTypeView
-from levelupapi.views import GameView, Game
+from levelupapi.views import (
+    register_user, 
+    login_user,
+    GameTypeView,
+    GameView,
+    # EventView,
+)
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
-router.register(r'games', GameView, 'games')
+router.register(r'games', GameView, 'game')
+# router.register(r'events', EventView, 'event')
 
-
-router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'gametypes', GameTypeView, 'gametypes')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
+    path('admin/', admin.site.urls),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('gametypes', GameTypeView.as_view({'get': 'name'}), name='game_type'),
+    path('games', GameView, name="game")
 ]
-
-
-urlpatterns = [
-    path('register', register_user),
-    path('login', login_user),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
-]
-
