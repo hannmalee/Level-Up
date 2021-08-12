3951032fa1a01baf1692e0a4b538a44b30eec011
hann <hmweeks93@gmail.com>
2021-08-12 16:01:40 -0500

    events

--- levelup/urls.py
+++ levelup/urls.py
@@ -25,13 +25,13 @@ from levelupapi.views import (
     login_user,
     GameTypeView,
     GameView,
-    # EventView,
+    EventView,
 )
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypeView, 'gametype')
 router.register(r'games', GameView, 'game')
-# router.register(r'events', EventView, 'event')
+router.register(r'events', EventView, 'event')
 
 
 urlpatterns = [
